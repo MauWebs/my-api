b123dc4180b9e9a78cf78b02cfdf2ec59daacd0e
MauWebs <mauriciovidalwebs@gmail.com>
2023-01-25 15:28:20 -0300

    commit Settings

--- app/models.py
+++ app/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    technology = models.CharField(max_length=200)
+    technology = models.CharField(max_length=200, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
